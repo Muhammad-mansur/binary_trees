0012756162c9edca475373a7f68b59b69abe0c9a
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-25 15:13:56 -0700

    10

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,5 +17,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
 
-	return ((left_h > right_h) ? left_h : right_h) +1;
+	return ((left_h > right_h) ? left_h + 1 : right_h + 1);
 }
