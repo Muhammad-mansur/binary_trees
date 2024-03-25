a85c869e2ee2860a4929997e203483c69b64a1a3
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-25 15:03:58 -0700

    9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to be measured.
+ *
+ * Return: the height of the tree.
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return ((left_h > right_h) ? left_h : right_h) +1;
+}
