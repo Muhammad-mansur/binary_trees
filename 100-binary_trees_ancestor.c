79877e1d529604fb0da4cf8096c389d469aaeec6
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-26 13:30:36 -0700

    100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,50 +1,42 @@
 #include "binary_trees.h"
 
 /**
- * find_ancestor - Finds the ancestor of a given node
- * @node: Pointer to the node
- * @ancestor: Pointer to store the ancestor
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
  *
- * Return: Void
+ * Return: Pointer to the lowest common ancestor node of the two given nodes,
+ *         or NULL if no common ancestor was found
  */
-void find_ancestor(const binary_tree_t *node, binary_tree_t **ancestor)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    while (node)
+    const binary_tree_t *temp_first = first;
+    const binary_tree_t *temp_second = second;
+
+    while (temp_first)
     {
-        *ancestor = (binary_tree_t *)node;
-        node = node->parent;
-    }
-}
+        const binary_tree_t *ancestor = second;
 
-/**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * @first: pointer to the first node
- * @second: pointer to the second node
- *
- * Return: pointer to the lowest common ancestor node of the two given nodes,
- * or NULL if no common ancestor was found
- */
-
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
-{
-    binary_tree_t *ancestor1 = NULL, *ancestor2 = NULL;
-
-    find_ancestor(first, &ancestor1);
-    find_ancestor(second, &ancestor2);
-
-    while (ancestor1)
-    {
-        binary_tree_t *temp;
-
-	temp = ancestor2;
-        while (temp)
+        while (ancestor)
         {
-            if (ancestor1 == temp)
-                return (ancestor1);
-            temp = temp->parent;
+            if (temp_first == ancestor)
+                return ((binary_tree_t *)temp_first);
+            ancestor = ancestor->parent;
         }
-        ancestor1 = ancestor1->parent;
+        temp_first = temp_first->parent;
+    }
+
+    while (temp_second)
+    {
+        const binary_tree_t *ancestor = first;
+
+        while (ancestor)
+        {
+            if (temp_second == ancestor)
+                return ((binary_tree_t *)temp_second);
+            ancestor = ancestor->parent;
+        }
+        temp_second = temp_second->parent;
     }
 
     return (NULL);
