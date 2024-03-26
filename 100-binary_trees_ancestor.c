9a8b1e36a8e2f0c50b12766d5f1b4dd149d746d5
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-26 13:39:44 -0700

    100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,36 +8,37 @@
  * Return: Pointer to the lowest common ancestor node of the two given nodes,
  *         or NULL if no common ancestor was found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    const binary_tree_t *temp_first = first;
-    const binary_tree_t *temp_second = second;
+	const binary_tree_t *temp_first = first;
+	const binary_tree_t *temp_second = second;
 
-    while (temp_first)
-    {
-        const binary_tree_t *ancestor = second;
+	while (temp_first)
+	{
+		const binary_tree_t *ancestor = second;
 
-        while (ancestor)
-        {
-            if (temp_first == ancestor)
-                return ((binary_tree_t *)temp_first);
-            ancestor = ancestor->parent;
-        }
-        temp_first = temp_first->parent;
-    }
+		while (ancestor)
+		{
+			if (temp_first == ancestor)
+				return ((binary_tree_t *)temp_first);
+			ancestor = ancestor->parent;
+		}
+		temp_first = temp_first->parent;
+	}
 
-    while (temp_second)
-    {
-        const binary_tree_t *ancestor = first;
+	while (temp_second)
+	{
+		const binary_tree_t *ancestor = first;
 
-        while (ancestor)
-        {
-            if (temp_second == ancestor)
-                return ((binary_tree_t *)temp_second);
-            ancestor = ancestor->parent;
-        }
-        temp_second = temp_second->parent;
-    }
+		while (ancestor)
+		{
+			if (temp_second == ancestor)
+				return ((binary_tree_t *)temp_second);
+			ancestor = ancestor->parent;
+		}
+		temp_second = temp_second->parent;
+	}
 
-    return (NULL);
+	return (NULL);
 }
